6b7b9b9b48eedac932bf2182adfa693621947eff
Mohamed Sherif <msahmed652@gmail.com>
2019-09-25 11:07:29 +0200

    fixed routes

--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const messageController = require('./../controllers/message');
 
 router.post('', messageController.addMessage);
-router.delete('', messageController.deleteMessage);
+router.delete('/:id', messageController.deleteMessage);
 router.put('', messageController.editMessage);
 router.get('', messageController.getMessages);
 
--- backend/routes/reply.js
+++ backend/routes/reply.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const replyController = require('./../controllers/reply');
 
+router.get('/:id', replyController.getReplies);
 router.post('/add', replyController.replyToMessage);
 
 module.exports = router;
